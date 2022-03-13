24b23530b8ec13bb4623caa7d953f52d389030e3
jananikrishnan1 <janani.krishnan@kellogg.northwestern.edu>
2022-03-13 20:50:37 +0000

    2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,45 @@
 class PostsController < ApplicationController
 
   def new
+    @posts = Post.where({ user_id: session[:user_id] })
     @post = Post.new
     @post.place_id = params["place_id"]
+    
   end
 
   def create
-    @post = Post.new(params["post"])
-    @post.save
+    if @current_user
+      @post = Post.new(params["post"])
+      @post.save
+    end
+
     redirect_to "/places/#{@post.place.id}"
+
   end
+
+#-------------
+#def index
+#  @tasks = Task.where({ user_id: session[:user_id] })
+#  @task = Task.new
+#end
+
+#def create
+#  if @current_user
+#    @task = Task.new(params["task"])
+#    @task.save
+#  end
+  
+#  redirect_to "/tasks"
+#end
+
+#def destroy
+#  @task = Task.find(params["id"])
+#  if @current_user && @task.user_id == @current_user.id
+#    @task.destroy
+#  end
+
+#  redirect_to "/tasks"
+#end
+#---------
 
 end
